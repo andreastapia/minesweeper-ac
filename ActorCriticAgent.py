removed reward normalization, fix loss propagation

ActorCriticAgent.py
@@ -133,24 +133,28 @@ class ActorCriticAgent:
 
         returns = torch.tensor(returns)
 
-        returns = (returns - returns.mean()) / (returns.std() + self.eps)
+        #normalized returns, may affect training when winning suddenly
+        #returns = (returns - returns.mean()) / (returns.std() + self.eps)
         
         for log_prob, value, R in zip(self.saved_log_probs, self.saved_values, returns):
             advantage = R - value
+            advantage = advantage.detach()
 
             # calculate actor (policy) loss
             actor_loss.append(-log_prob * advantage)
 
-            # calculate critic (value) loss using MSE smooth loss
+            # calculate critic (value) loss using MSE 
             critic_loss.append(F.mse_loss(value[0], R))
 
         self.optimizer_actor.zero_grad()
         self.optimizer_critic.zero_grad()
         loss_actor = torch.stack(actor_loss).sum()
         loss_critic = torch.stack(critic_loss).sum()
-        loss = loss_actor + loss_critic
-        loss.backward()
-        self.optimizer_actor.step()
+        
+        loss_actor.backward()
+        loss_critic.backward() 
+
+        self.optimizer_actor.step()               
         self.optimizer_critic.step()
 
         self.selected_actions = []

main.py
@@ -17,15 +17,15 @@ def main():
 
     env = MinesweeperDiscrete()
 
-    total_num_episodes = int(100000)  # Total number of episodes
+    total_num_episodes = int(20000)  # Total number of episodes
 
     #input_channels, conv_hidden, output_channels, learning_rate, gammaS
-    agent = ActorCriticAgent(11,64,81,0.0001,0.99)
+    agent = ActorCriticAgent(11,64,81,0.001,0.99)
 
     rewards = []
     steps = []
     for episode in range(total_num_episodes):
-        print(episode)
+        #print(episode)
         episode_steps = 0
         obs, info = env.reset()
         done = False
@@ -44,12 +44,13 @@ def main():
         steps.append(episode_steps)
 
         if episode % 1000 == 0:
-            filename = "trained_model_{episode}.pt"
-            torch.save(agent, filename.format(episode=episode))
             avg_reward = int(np.mean(rewards))
             avg_steps = int(np.mean(steps))
             print("Episode:", episode, "Average Reward:", avg_reward, "Current Average Steps:", avg_steps)
 
+        if episode % 10000 == 0 and episode != 0:
+            filename = "trained_model_{episode}.pt"
+            torch.save(agent, filename.format(episode=episode))
 
     # # Guardar
     torch.save(agent, 'trained_model.pt')